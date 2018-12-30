e89b430193183c9aeb0fdbc7579a7333b0cc0b56
Evan Stoll <evanjsx@gmail.com>
2018-12-30 13:18:26 -0500

    Add rustdoc for Problem One

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
+/// Multiples of 3 and 5
+/// Problem 1
+/// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+///
+/// Find the sum of all the multiples of 3 or 5 below 1000.
 fn one() -> u32 {
     (0..)
         .take_while(|&n| n < 1000)
         .filter(|&n| n % 3 == 0 || n % 5 == 0)
         .sum()
 }
-
 
 #[cfg(test)]
 mod tests {

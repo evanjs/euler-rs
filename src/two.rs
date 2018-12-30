4bb5a95d562c1efe2441e5679c32870846c17f89
Evan Stoll <evanjsx@gmail.com>
2018-12-30 15:07:56 -0500

    Run `cargo fmt`

--- src/two.rs
+++ src/two.rs
@@ -7,7 +7,6 @@
 //! By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 //!
 
-
 /// # The Golden Ratio
 /// ## Notes:
 /// See: https://en.wikipedia.org/wiki/Golden_ratio
